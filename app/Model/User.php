e082ea531b4218932f6f821c8f05dda4d398174e
xingobar <gary840227@gmail.com>
2021-10-06 22:29:28 +0800

    feat: 新建 users table
    1. 新建 users table

---
+++ app/Model/User.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Model;
+
+use Hyperf\Database\Model\SoftDeletes;
+
+class User extends Model
+{
+    use SoftDeletes;
+
+    public const GENDER_MALE = 'MALE';
+
+    public const GENDER_FEMALE = 'FEMALE';
+
+    public static $genderMap = [
+        self::GENDER_FEMALE => '女性',
+        self::GENDER_MALE => '男性',
+    ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'account', 'email', 'avatar', 'password', 'gender', 'verified_at', 'verify_token'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
+}
---
+++ migrations/2021_10_05_155546_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+use Hyperf\Database\Migrations\Migration;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Schema\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('account')->comment('帳號');
+            $table->string('avatar')->nullable()->comment('頭像');
+            $table->string('email')->comment('信箱');
+            $table->string('password')->comment('密碼');
+            $table->string('gender')->default(\App\Model\User::GENDER_MALE)->comment('性別');
+            $table->timestamp('verified_at')->nullable()->comment('驗證時間');
+            $table->string('verify_token')->nullable()->comment('驗證 token');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+}
