7d169029bd5921a50e2093afbba9edec9c155891
xingobar <gary840227@gmail.com>
2021-10-08 22:35:00 +0800

    feat: 更改會員資料
    1. 更改會員資料 api

---
+++ app/Controller/MeController.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Controller;
+
+use App\Contracts\UserServiceInterface;
+use App\Mail\ConfirmEmail;
+use App\Middleware\AuthenticateMiddleware;
+use App\Request\UpdateUserRequest;
+use App\Resource\UserResource;
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\HttpServer\Annotation\Controller;
+use Hyperf\HttpServer\Annotation\Middleware;
+use Hyperf\HttpServer\Annotation\PutMapping;
+use Hyperf\Utils\Str;
+use HyperfExt\Hashing\Hash;
+use HyperfExt\Mail\Mail;
+
+/**
+ * @Controller(prefix="me")
+ * Class MeController
+ */
+class MeController extends AbstractController
+{
+    /**
+     * @Inject
+     * @var UserServiceInterface
+     */
+    public $userService;
+
+    /**
+     * @Middleware(AuthenticateMiddleware::class)
+     * @PutMapping(path="")
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function update(UpdateUserRequest $request)
+    {
+        // 取得過濾過的資料
+        $params = $request->validated();
+
+        // 因為更改信箱, 所以要重新驗證
+        if (isset($params['email'])) {
+            // 寄送信箱驗證信件
+            Mail::to($params['email'])->queue(new ConfirmEmail());
+            auth()->user()->update([
+                'confirm_token' => Str::random(32),
+            ]);
+        }
+
+        // 密碼重新加密
+        if (isset($params['password'])) {
+            $params['password'] = Hash::make($params['password']);
+        }
+
+        auth()->user()->fill($params)->save();
+
+        return (new UserResource(auth()->user()))->toResponse();
+    }
+}
---
+++ app/Request/UpdateUserRequest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Request;
+
+use App\Model\User;
+use Hyperf\Validation\Request\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        $array = [];
+        if ($this->has('email')) {
+            $array['email'] = 'required|email|unique:users,email,' . auth()->user()->id . ',id';
+        }
+
+        if ($this->has('gender')) {
+            $array['gender'] = 'required|in:' . implode(',', array_keys(User::$genderMap));
+        }
+
+        if ($this->has('password')) {
+            $array['password'] = 'required|min:6';
+            $array['confirm_password'] = 'required|same:password';
+        }
+
+        if ($this->has('avatar')) {
+            $array['avatar'] = 'required';
+        }
+
+        return $array;
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => '請輸入電子信箱',
+            'email.email' => '電子信箱格式不符',
+            'email.unique' => '電子信箱已存在',
+            'gender.required' => '請輸入性別',
+            'gender.in' => '傳入的性別有誤',
+            'password.required' => '請輸入密碼',
+            'password.min' => '密碼至少 :min 位',
+            'confirm_password.required' => '請輸入確認密碼',
+            'confirm_password.same' => '密碼不一致',
+            'avatar.required' => '請傳入大頭貼',
+        ];
+    }
+}
